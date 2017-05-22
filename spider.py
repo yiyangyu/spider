f4aaa58d791681f4ccfdfe4adc8c25ca36a3d3cb
yiyangyu <yiyangyuyu@gmail.com>
2017-05-22 14:40:51 +0800

    init

---
+++ spider.py
@@ -0,0 +1,143 @@
+import os
+import requests
+from pyquery import PyQuery as pq
+
+
+class Model(object):
+    """
+    基类, 用来显示类的信息
+    """
+    def __repr__(self):
+        name = self.__class__.__name__
+        properties = ('{}=({})'.format(k, v) for k, v in self.__dict__.items())
+        s = '\n<{} \n  {}>'.format(name, '\n  '.join(properties))
+        return s
+
+
+class Movie(Model):
+    """
+    存储电影信息
+    """
+    def __init__(self):
+        self.name = ''
+        self.score = 0
+        self.quote = ''
+        self.cover_url = ''
+        self.ranking = 0
+
+
+def cached_url(url):
+    """
+    缓存, 避免重复下载网页浪费时间
+    """
+    folder = 'cached'
+    filename = url.split('=', 1)[-1] + '.html'
+    'cached/0.html'
+    path = os.path.join(folder, filename)
+    # print('path', path)
+    if os.path.exists(path):
+        with open(path, 'rb') as f:
+            s = f.read()
+            return s
+    else:
+        # 建立 cached 文件夹
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+
+        headers = {
+            'user-agent': '''Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.98 Safari/537.36
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8''',
+        }
+        # 发送网络请求, 把结果写入到文件夹中
+        # headers是模拟浏览器发送请求，也可以加入cookie字段
+        r = requests.get(url, headers)
+
+        with open(path, 'wb') as f:
+            f.write(r.content)
+        return r.content
+
+
+def movie_from_div(div):
+    """
+    从一个 div 里面获取到一个电影信息
+    """
+    # 解析出整个div部分
+    e = pq(div)
+    # print('1234', e)
+
+    # 小作用域变量用单字符
+    m = Movie()
+    m.name = e('.title').text()
+    m.score = e('.rating_num').text()
+    m.quote = e('.inq').text()
+    # 获取<img>标签中的属性’src‘的值
+    m.cover_url = e('img').attr('src')
+    # 获取class=’pic‘标签中的子节点<em>的innertext
+    m.ranking = e('.pic').find('em').text()
+
+    return m
+
+
+def movies_from_url(url):
+    """
+    从 url 中下载网页并解析出页面内所有的电影
+    """
+    '''
+    只会下载一次
+    '''
+    # 获取整个html,有cache就不用下载,直接从本地读取
+    page = cached_url(url)
+    # print('page', type(page))
+    # print(page.decode())
+    '''
+    1. 解析 dom
+    2. 找到父亲节点
+    3. 每个子节点拿一个movie
+    '''
+    # 解析出整个HTML文件, page是html页面(或者html的一部分)的bytes形式
+    e = pq(page)
+    # print('e', type(e))
+    # print('1e1e', e)
+    # 2.父节点
+    # 解析出包含’class=item‘的部分, e是pq(page),其中的page是html页面(或者html的一部分)的bytes形式
+    items = e('.item')
+    # print('items', type(items))
+    # print('2w2w', items)
+    # 调用 movie_from_div
+    # list comprehension
+    movies = [movie_from_div(i) for i in items]
+    return movies
+
+
+def download_image(url):
+    folder = "img"
+    name = url.split("/")[-1]
+    path = os.path.join(folder, name)
+
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+    if os.path.exists(path):
+        return
+
+    headers = {
+        'user-agent': '''Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.98 Safari/537.36
+    Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8''',
+    }
+    # 发送网络请求, 把结果写入到文件夹中
+    r = requests.get(url, headers)
+    with open(path, 'wb') as f:
+        f.write(r.content)
+    return
+
+
+def main():
+    for i in range(0, 250, 25):
+        url = 'https://movie.douban.com/top250?start={}'.format(i)
+        movies = movies_from_url(url)
+        print('top250 movies', movies)
+        [download_image(m.cover_url) for m in movies]
+
+
+if __name__ == '__main__':
+    main()
